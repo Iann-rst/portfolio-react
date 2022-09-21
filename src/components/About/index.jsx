9007609fbf2d562abc8707949e7b1373353c9aae
Iann-rst <iannrst11@gmail.com>
2022-09-21 15:24:23 -0300

    feat: section About

---
+++ src/components/About/index.jsx
@@ -0,0 +1,38 @@
+import { Link, GithubLogo } from 'phosphor-react'
+import './styles.css';
+
+export function About() {
+  return (
+    <section className="about" id="about">
+      <div className="a-left">
+        <div className="a-avatar">
+          <img src="https://github.com/Iann-rst.png" alt="" />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1>Sobre Mim</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Eveniet facilis minima expedita, vero eaque iusto placeat officia necessitatibus error nostrum dignissimos accusamus maiores provident!
+          Autem, eveniet. Doloribus autem amet nesciunt.
+        </p>
+
+        <div className='social-btn'>
+          <a href="https://www.github.com/Iann-rst" target="_blank">
+            <button className='btn a-button'>
+              <GithubLogo size={24} weight="bold" />
+              Github
+            </button>
+          </a>
+
+          <a href="https://www.linkedin.com/in/iann-rodrigues-b71b9220a/" target="_blank">
+            <button className='btn a-button'>
+              <Link size={24} weight="bold" />
+              Currículo
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
