1a29b27bfec8e3192106100e1c79a7381dd983d3
Iann-rst <iannrst11@gmail.com>
2022-09-22 11:29:01 -0300

    fix: Portfólio

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -15,48 +15,46 @@ export function Projects() {
   }, [])
 
   return (
-    <section data-aos="fade" className='projects' id="portfolio">
-      <div className='p-container'>
-        <h1>
-          Projetos
-        </h1>
+    <section data-aos="fade" className='p-container' id="portfolio">
+      <h1>
+        Projetos
+      </h1>
 
-        <div className="p-wrapper">
-          <div className="p-card">
-            <h1>Nome do Projeto</h1>
-            <p>detalhes</p>
-            <a href="#"><button className='btn'>GitHub</button></a>
-          </div>
+      <div className="p-wrapper">
+        <div className="p-card">
+          <h1>Nome do Projeto</h1>
+          <p>detalhes</p>
+          <a href="#"><button className='btn'>GitHub</button></a>
+        </div>
 
-          <div className="p-card">
-            <h1>Nome do Projeto 2</h1>
-            <p>detalhes</p>
-            <a href="#"><button className='btn'>GitHub</button></a>
-          </div>
+        <div className="p-card">
+          <h1>Nome do Projeto 2</h1>
+          <p>detalhes</p>
+          <a href="#"><button className='btn'>GitHub</button></a>
+        </div>
 
-          <div className="p-card">
-            <h1>Nome do Projeto 3</h1>
-            <p>detalhes</p>
-            <a href="#"><button className='btn'>GitHub</button></a>
-          </div>
+        <div className="p-card">
+          <h1>Nome do Projeto 3</h1>
+          <p>detalhes</p>
+          <a href="#"><button className='btn'>GitHub</button></a>
+        </div>
 
-          <div className="p-card">
-            <h1>Nome do Projeto 4</h1>
-            <p>detalhes</p>
-            <a href="#"><button className='btn'>GitHub</button></a>
-          </div>
+        <div className="p-card">
+          <h1>Nome do Projeto 4</h1>
+          <p>detalhes</p>
+          <a href="#"><button className='btn'>GitHub</button></a>
+        </div>
 
-          <div className="p-card">
-            <h1>Nome do Projeto 5</h1>
-            <p>detalhes</p>
-            <a href="#"><button className='btn'>GitHub</button></a>
-          </div>
+        <div className="p-card">
+          <h1>Nome do Projeto 5</h1>
+          <p>detalhes</p>
+          <a href="#"><button className='btn'>GitHub</button></a>
+        </div>
 
-          <div className="p-card">
-            <h1>Nome do Projeto 6</h1>
-            <p>detalhes</p>
-            <a href="#"><button className='btn'>GitHub</button></a>
-          </div>
+        <div className="p-card">
+          <h1>Nome do Projeto 6</h1>
+          <p>detalhes</p>
+          <a href="#"><button className='btn'>GitHub</button></a>
         </div>
       </div>
     </section>
