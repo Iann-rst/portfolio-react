120678f3f06138c3b32210de6e66fd0fa50d0a20
Iann-rst <iannrst11@gmail.com>
2022-09-22 17:07:10 -0300

    fix: css portfólio

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -6,6 +6,7 @@ import 'aos/dist/aos.css';
 import './styles.css';
 
 import Projetos from '../../utils/projetos';
+import { GithubLogo } from 'phosphor-react';
 
 export function Projects() {
 
@@ -27,10 +28,68 @@ export function Projects() {
       <div className="p-wrapper">
 
         <div className="p-card">
+          <h1>{Projetos[0].title}</h1>
+          <p>{Projetos[0].description}</p>
+          <a href={`${Projetos[0].link}`} target="_blank">
+            <button className='btn'>
+              <GithubLogo size={18} weight="bold" />
+              GitHub
+            </button>
+          </a>
+        </div>
+
+        <div className="p-card">
+          <h1>{Projetos[1].title}</h1>
+          <p>{Projetos[1].description}</p>
+          <a href={`${Projetos[1].link}`} target="_blank">
+            <button className='btn'>
+              <GithubLogo size={18} weight="bold" />
+              GitHub
+            </button>
+          </a>
+        </div>
+
+        <div className="p-card">
+          <h1>{Projetos[2].title}</h1>
+          <p>{Projetos[2].description}</p>
+          <a href={`${Projetos[2].link}`} target="_blank">
+            <button className='btn'>
+              <GithubLogo size={18} weight="bold" />
+              GitHub
+            </button>
+          </a>
+        </div>
+
+        <div className="p-card">
+          <h1>{Projetos[3].title}</h1>
+          <p>{Projetos[3].description}</p>
+          <a href={`${Projetos[3].link}`} target="_blank">
+            <button className='btn'>
+              <GithubLogo size={18} weight="bold" />
+              GitHub
+            </button>
+          </a>
+        </div>
+
+        <div className="p-card">
+          <h1>{Projetos[4].title}</h1>
+          <p>{Projetos[4].description}</p>
+          <a href={`${Projetos[4].link}`} target="_blank">
+            <button className='btn'>
+              <GithubLogo size={18} weight="bold" />
+              GitHub
+            </button>
+          </a>
+        </div>
+
+        <div className="p-card">
           <h1>{Projetos[5].title}</h1>
           <p>{Projetos[5].description}</p>
           <a href={`${Projetos[5].link}`} target="_blank">
-            <button className='btn'>GitHub</button>
+            <button className='btn'>
+              <GithubLogo size={18} weight="bold" />
+              GitHub
+            </button>
           </a>
         </div>
 
