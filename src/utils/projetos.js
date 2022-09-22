24cc1017a764d4cde753910a17195b4ddd22a026
Iann-rst <iannrst11@gmail.com>
2022-09-22 16:14:44 -0300

    feat: Projetos

---
+++ src/utils/projetos.js
@@ -0,0 +1,40 @@
+const Projetos = [
+  {
+    id: 1,
+    link: 'https://github.com/Iann-rst/nlw_eSports',
+    title: 'NLW eSports',
+    description: 'Projeto desenvolvido durante a NLW, com foco no eSports e com o objetivo de ajudar gamers a encontrar Duos (parceiros) para jogarem juntos seus games favoritos.'
+  },
+  {
+    id: 2,
+    link: 'https://github.com/Iann-rst/gopizza',
+    title: 'GoPizza',
+    description: 'GoPizza: Aplicativo desenvolvido em React Native para auxiliar Garçons de uma Pizzaria.'
+  },
+  {
+    id: 3,
+    link: 'https://github.com/Iann-rst/rocketHelp',
+    title: 'Rocket Help',
+    description: 'Rocket Help é uma aplicação desenvolvida em react native que permite gerenciar em tempo real solicitações de manutenção e suporte a equipamentos.'
+  },
+  {
+    id: 4,
+    link: 'https://github.com/Iann-rst/Sistema-de-chamadas',
+    title: 'Sistema de Chamados',
+    description: 'Criação de um sistema de chamadas em ReactJS e integração com o Firebase.'
+  },
+  {
+    id: 5,
+    link: 'https://github.com/Iann-rst/Letmeask',
+    title: 'Letmeask: Crie salas de Q&A ao-vivo.',
+    description: `App desenvolvido durante a NLW Together na trilha de desenvolvimento Mission: ReactJS.`
+  },
+  {
+    id: 6,
+    link: 'https://github.com/Iann-rst/portfolio-react',
+    title: 'Portfólio com ReactJS',
+    description: 'Criando um website Portfólio com ReactJS'
+  },
+]
+
+export default Projetos;
